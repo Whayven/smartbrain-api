d92e07dc6c9cb9d731b22c79a9b83bdb5a5ef4a5
Whayven <foster_wayne@columbusstate.edu>
2019-07-11 12:30:51 -0400

    updating database

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-curved-47788',
         user: 'wayne',
-        password: 'amber',
+        password: '',
         database: 'smart-brain'
     }
 });
